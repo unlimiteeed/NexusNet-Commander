19efd95bdee11a5d0b890927bd8b264e271302ff
unlimiteeed <benyamin.mohamdian@gmail.com>
2024-04-12 03:55:10 -0700

    banner

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 
 	connect "lbot.init/project/cmd/connect"
 	run "lbot.init/project/cmd/run"
+	banner "lbot.init/project/pkg/banner"
 	clear "lbot.init/project/pkg/clear"
 )
 
 func main() {
 	clear.ClearTerminal()
+	banner.Pbanner()
 	fmt.Printf("\n[+]Running Command\n")
 	excutec := "whoami"
 	customPort := 5555
---
+++ pkg/banner/banner.go
@@ -0,0 +1,16 @@
+package banner
+
+import (
+	"fmt"
+)
+
+func Pbanner() {
+	fmt.Println("  _   _                     _   _      _   ")
+	fmt.Println(" | \\ | |                   | \\ | |    | |  ")
+	fmt.Println(" |  \\| | _____  ___   _ ___|  \\| | ___| |_ ")
+	fmt.Println(" | . ` |/ _ \\ \\/ / | | / __| . ` |/ _ \\ __|")
+	fmt.Println(" | |\\  |  __/>  <| |_| \\__ \\ |\\  |  __/ |_ ")
+	fmt.Println(" |_| \\_|\\___/_/\\_\\\\__,_|___/_| \\_|\\___|\\__|")
+	fmt.Println("                                           ")
+	fmt.Println("                                           ")
+}
